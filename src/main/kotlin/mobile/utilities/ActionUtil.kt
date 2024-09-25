fed03efcad05e33fb3fe38e40ef4874abd0893a2
eck.song92 <eck.song92@rainist.com>
2024-09-25 20:45:11 +0900

    Clear Git cache and re-add all files

---
+++ src/main/kotlin/mobile/utilities/ActionUtil.kt
@@ -0,0 +1,25 @@
+package mobile.utilities
+
+import org.openqa.selenium.WebElement
+
+interface ActionUtil : WaitUtil {
+
+    /**
+     * Tap Action
+     */
+    fun tap(element: WebElement) {
+
+
+    }
+
+    fun tapElement(element: WebElement) {
+        element.click()
+        println("Tapping element: $element")
+    }
+
+    fun typeElement(element: WebElement, text: String) {
+        element.clear()
+        element.sendKeys(text)
+        println("Typing text: $text to element: $element")
+    }
+}
